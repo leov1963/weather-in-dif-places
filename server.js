454b775e0213c03deb718ba4f53dbfad8b7d0f49
leov1963 <leov1963@gmail.com>
2021-02-10 22:24:54 -0800

    Add Routes for /new

--- server.js
+++ server.js
@@ -23,7 +23,19 @@ app.get('/', async(req, res) => {
         } catch(e) {
         console.log(e)
     };
+})
 
+app.get("/new", (req,res) => {
+    res.render("new.ejs")
+})
+
+app.post('/new', (req, res) => {
+    console.log(req.body);
+    db.place.create(req.body)
+    .then((createdPlace)=> {
+        console.log('Created Place = ', createdPlace);
+        res.redirect('/');
+    });
 })
 
 let myWeather = []
@@ -34,6 +46,8 @@ app.get("/:zipcode", (req, res) => {
         res.send(`The current temperature in your area is: ${myWeather[0].current.temperature} degrees fahrenheit. The sky is ${myWeather[0].current.skytext}<br><a href="/">return to home</a>.`)
     })
 })
+
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
