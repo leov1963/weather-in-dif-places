3c82f26a871af6c7f1ff30aaa1ad2fc25867122b
leov1963 <leov1963@gmail.com>
2021-02-10 19:17:06 -0800

    Add server.js - Update server.js with starter code

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const axios = require('axios');
+const ejsLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+// const db = require("./models")
+
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(ejsLayouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.send("hello")
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
